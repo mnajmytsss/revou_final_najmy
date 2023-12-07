52b89b9ecf71d58fb14c43bc7f2c739dc7f11dfb
mnajmytsss <najmytsaqiby22@gmail.com>
2023-12-07 13:00:11 +0700

    update: add routes for doctor register

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,14 @@ const { Router } = require("express");
 const dashboardRoutes = require("./dashboardRoutes");
 const preventAttackController = require("./preventAttackRoutes");
 const authRouter = require('./authRoutes')
+const doctorRouter = require('./doctorRoutes')
+const authenticationMiddleware = require('../middleware/authenticationMiddleware')
 
 const router = Router();
 
 router.use("/", dashboardRoutes);
 router.use("/api/v1/attack", preventAttackController)
 router.use("/auth", authRouter)
+router.use("/api/v1/doctor", authenticationMiddleware, doctorRouter)
 
 module.exports = router;
