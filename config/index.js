323b3fdb8cedec769f7e3b71d478542d005458c6
mnajmytsss <najmytsaqiby22@gmail.com>
2023-12-07 12:51:29 +0700

    feat: add new func: BCRYPT_SALT_ROUNDS

--- config/index.js
+++ config/index.js
@@ -1,9 +1,11 @@
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
 
-dotenv.config()
+dotenv.config();
 
-const JWT_SIGN = process.env.JWT_SIGN
+const JWT_SIGN = process.env.JWT_SIGN;
+const BCRYPT_SALT_ROUNDS = parseInt(process.env.BCRYPT_SALT_ROUNDS) || 10; 
 
 module.exports = {
-  JWT_SIGN
-}
+  JWT_SIGN,
+  BCRYPT_SALT_ROUNDS,
+};
