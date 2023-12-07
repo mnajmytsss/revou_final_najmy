8144cb8449ca8a9ca031ab24efdfced6b72c5424
mnajmytsss <najmytsaqiby22@gmail.com>
2023-12-07 12:57:34 +0700

    update: authorizationForRole1,2,3

--- middleware/authorizationMiddleware.js
+++ middleware/authorizationMiddleware.js
@@ -1,27 +1,35 @@
-const jwt = require('jsonwebtoken')
-const { JWT_SIGN } = require('../config/index')
+const jwt = require('jsonwebtoken');
+const { JWT_SIGN } = require('../config/index');
 
-const authorizationMiddlewareAll = (req, res, next) => {
-  const authHeader = req.headers.authorization
-  
+const authorizationMiddleware = (allowedRoles) => (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
   if (!authHeader) {
-    res.status(401).json({ error: 'Unauthorized' })
-  } else {
-    const token = authHeader.split(' ')[1]
-    
-    try {
-      const decodedToken = jwt.verify(token, JWT_SIGN)
-      if (decodedToken.role === 'admin' || decodedToken.role === "user") {
-        next()
-      } else {
-        res.status(401).json({ error: 'Unauthorized' })
-      }
-    } catch (error) {
-      res.status(400).json({ error: error.message })
-    }
+    return res.status(401).json({ error: 'Unauthorized' });
   }
-}
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decodedToken = jwt.verify(token, JWT_SIGN);
+
+    // Check if the decoded token's role is included in the allowed roles
+    if (allowedRoles.includes(decodedToken.role)) {
+      next();
+    } else {
+      return res.status(403).json({ error: 'Forbidden' });
+    }
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
+
+const authorizationMiddlewareForRole1 = authorizationMiddleware(['1']);
+const authorizationMiddlewareForRole2 = authorizationMiddleware(['2']);
+const authorizationMiddlewareForRole3 = authorizationMiddleware(['3']);
 
 module.exports = {
-    authorizationMiddlewareAll
-}
+  authorizationMiddlewareForRole1,
+  authorizationMiddlewareForRole2,
+  authorizationMiddlewareForRole3
+};
