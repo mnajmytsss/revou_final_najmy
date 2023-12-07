70923068baf9e6252d7147e9b63002a3006c0cc0
mnajmytsss <najmytsaqiby22@gmail.com>
2023-12-07 13:59:12 +0700

    feat: routes for getDoctorById

--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -1,10 +1,11 @@
 const { Router } = require("express")
-const { registerDoctor, getAllDoctors } = require("../controller/doctorController")
+const { registerDoctor, getAllDoctors, getDoctorById } = require("../controller/doctorController")
 const { authorizationMiddlewareForRole2 } = require('../middleware/authorizationMiddleware')
 
 const doctorRouter = Router()
 
 doctorRouter.post("/register", authorizationMiddlewareForRole2, registerDoctor)
 doctorRouter.get("/getAllDoctors", getAllDoctors)
+doctorRouter.get("/getDoctorById/:dok_id", getDoctorById)
 
 module.exports = doctorRouter
