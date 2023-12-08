793bde94e71b9b66bb21919129cd4212c815d35f
mnajmytsss <najmytsaqiby22@gmail.com>
2023-12-08 14:36:08 +0700

    feat: activate doctor account routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ const dashboardRoutes = require("./dashboardRoutes");
 const preventAttackController = require("./preventAttackRoutes");
 const authRouter = require('./authRoutes')
 const doctorRouter = require('./doctorRoutes')
-const authenticationMiddleware = require('../middleware/authenticationMiddleware')
+const authenticationMiddleware = require('../middleware/authenticationMiddleware');
+const actDoctorRouter = require("./activateDoctorAccount");
 
 const router = Router();
 
@@ -11,5 +12,6 @@ router.use("/", dashboardRoutes);
 router.use("/api/v1/attack", preventAttackController)
 router.use("/auth", authRouter)
 router.use("/api/v1/doctor", authenticationMiddleware, doctorRouter)
+router.use("/api/v1/admin", authenticationMiddleware, actDoctorRouter)
 
 module.exports = router;
