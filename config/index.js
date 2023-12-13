c1c46c09119828a9ce27b2e52a1fe2f18526f715
mnajmytsss <najmytsaqiby22@gmail.com>
2023-12-13 21:59:23 +0700

    feat: aws sdk config

--- config/index.js
+++ config/index.js
@@ -1,11 +1,28 @@
 const dotenv = require("dotenv");
+const AWS = require("aws-sdk");
+require("aws-sdk/lib/maintenance_mode_message").suppress = true;
 
 dotenv.config();
 
 const JWT_SIGN = process.env.JWT_SIGN;
 const BCRYPT_SALT_ROUNDS = parseInt(process.env.BCRYPT_SALT_ROUNDS) || 10; 
+const AWS_REGION = process.env.AWS_REGION;
+
+// Konfigurasi kredensial AWS
+// JS SDK v3 does not support global configuration.
+// Codemod has attempted to pass values to each service client in this file.
+// You may need to update clients outside of this file, if they use global config.
+AWS.config.update({
+  region: AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
 
 module.exports = {
   JWT_SIGN,
   BCRYPT_SALT_ROUNDS,
+  AWS_REGION,
 };
+
