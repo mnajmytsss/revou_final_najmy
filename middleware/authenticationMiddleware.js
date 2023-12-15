bddf0f3db0d30be62537e46515d1a4f7b5e1a4e7
mnajmytsss <najmytsaqiby22@gmail.com>
2023-12-16 01:28:47 +0700

    update: all error message to indonesian lang

--- middleware/authenticationMiddleware.js
+++ middleware/authenticationMiddleware.js
@@ -2,23 +2,24 @@ const jwt = require('jsonwebtoken');
 const { JWT_SIGN } = require('../config');
 const db = require('../db');
 
-async function middlewareAutentikasi(req, res, next) {
-  const authHeader = req.headers.authorization;
-  const token = authHeader.split(' ')[1];
+async function authenticationMiddleware(req, res, next) {
+  const authHeader = req.headers.authorization
+  const token = authHeader.split(' ')[1]
 
   if (!token) {
-    return res.status(401).json({ error: 'Token tidak tersedia' });
+    return res.status(401).json({ error: 'Token tidak ditemukan' });
   }
 
   try {
+    
     const decoded = jwt.verify(token, JWT_SIGN);
     if (!decoded.id) {
-      return res.status(401).json({ error: 'Token tidak valid - ID pengguna tidak tersedia' });
-    }
+        return res.status(401).json({ error: 'token salah - user ID tidak ditemukan' });
+      }
     const [user] = await db.execute('SELECT * FROM USERS WHERE user_id = ?', [decoded.id]);
 
     if (!user) {
-      return res.status(401).json({ error: 'Token tidak valid - Pengguna tidak ditemukan' });
+      return res.status(401).json({ error: 'token salah - User tidak ditemukan' });
     }
 
     // req.user = user[0].ROLE_ID;
@@ -26,8 +27,8 @@ async function middlewareAutentikasi(req, res, next) {
     next();
   } catch (error) {
     console.error(error);
-    res.status(401).json({ error: 'Token tidak valid - Verifikasi token gagal' });
+    res.status(401).json({ error: 'token salah - verifikasi token gagal' });
   }
 }
 
-module.exports = middlewareAutentikasi;
+module.exports = authenticationMiddleware;
