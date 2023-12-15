da4585df32dd6f9e90ca958a5ebed1a9a42769bd
mnajmytsss <najmytsaqiby22@gmail.com>
2023-12-16 01:20:28 +0700

    update: all error message to indonesian lang

--- controller/informerController.js
+++ controller/informerController.js
@@ -28,7 +28,7 @@ async function registerInformer(req, res) {
       );
 
     } catch (error) {
-      console.error("Error during user insertion:", error);
+      console.error("Error selama penyisipan pengguna:", error);
       throw error; // Untuk melihat stack trace lengkap pada console
     }
 
@@ -46,7 +46,7 @@ async function registerInformer(req, res) {
 
         res.status(201).json({ user_id: insertedUserId, inf_id: insertedInfId });
       } catch (error) {
-        console.error("Error during informer insertion:", error);
+        console.error("Error selama penyisipan informer:", error);
         throw error; // Untuk melihat stack trace lengkap pada console
       }
     } else {
@@ -60,7 +60,7 @@ async function registerInformer(req, res) {
 
 async function getAllInformer(req, res) {
   try {
-    // Fetch all informer from the INFORMER table
+    // Mengambil semua informer dari tabel INFORMER
     const [informer] = await db.execute('SELECT * FROM INFORMER');
 
     res.status(200).json({ informer });
@@ -74,17 +74,17 @@ async function getInformerById(req, res) {
   try {
     const { inf_id } = req.params;
 
-    // Check if inf_id is provided
+    // Memeriksa apakah inf_id disediakan
     if (!inf_id) {
-      return res.status(400).json({ error: 'Parameter inf_id is required.' });
+      return res.status(400).json({ error: 'Parameter inf_id diperlukan.' });
     }
 
-    // Fetch the informer with the specified ID from the INFORMER table
+    // Mengambil informer dengan ID yang spesifik dari tabel INFORMER
     const [informer] = await db.execute('SELECT * FROM INFORMER WHERE INF_ID = ?', [inf_id]);
 
-    // Check if the informer is found
+    // Memeriksa apakah informer ditemukan
     if (informer.length === 0) {
-      return res.status(404).json({ error: 'Informer tidak ditemukan' });
+      return res.status(404).json({ error: 'Informer tidak ditemukan.' });
     }
 
     res.status(200).json({ informer: informer[0] });
@@ -99,34 +99,34 @@ async function updateInformer(req, res) {
     const { inf_id } = req.params;
     const updateParams = req.body;
 
-    // Fetch the existing informer with the specified ID
+    // Mengambil informer yang ada dengan ID yang spesifik
     const existingInformer = await db.execute('SELECT * FROM INFORMER WHERE INF_ID = ?', [inf_id]);
     arrInf = existingInformer[0];
    
-    // Check if the informer is found
+    // Memeriksa apakah informer ditemukan
     if (arrInf.length === 0) {
-      return res.status(404).json({ error: 'Informer not found.' });
+      return res.status(404).json({ error: 'Informer tidak ditemukan.' });
     }
 
-    // Ensure that the authenticated user is the owner of the informer profile
+    // Memastikan bahwa pengguna yang terautentikasi adalah pemilik profil informer
     const decoded = jwt.verify(req.headers.authorization.split(' ')[1], JWT_SIGN);
     if (decoded.id !== arrInf[0].USER_ID) {
       return res.status(403).json({ error: 'Anda tidak memiliki izin untuk memperbarui informasi informer ini.' });
     }
-    ;
-    // Generate the SET part of the SQL query based on the provided updateParams
+
+    // Menghasilkan bagian SET dari kueri SQL berdasarkan updateParams yang diberikan
     const updateValues = Object.keys(updateParams)
       .map((key) => `${key} = ?`)
       .join(', ');
 
-    // Prepare the SQL query with dynamic SET values
+    // Menyiapkan kueri SQL dengan nilai SET dinamis
     const sqlQuery = `UPDATE INFORMER SET ${updateValues} WHERE INF_ID = ?`;
 
-    // Prepare the values for execution
+    // Menyiapkan nilai untuk dieksekusi
     const updateValuesArray = Object.values(updateParams);
     updateValuesArray.push(inf_id);
 
-    // Update the informer's information in the INFORMER table
+    // Memperbarui informasi informer di tabel INFORMER
     await db.execute(sqlQuery, updateValuesArray);
 
     res.status(200).json({ message: 'Informasi informer berhasil diperbarui.' });
