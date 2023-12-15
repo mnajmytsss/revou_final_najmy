67b8c8b2820970b6f31c65bb679abd589d54fd18
mnajmytsss <najmytsaqiby22@gmail.com>
2023-12-15 19:48:14 +0700

    fix: remove authorization for doctor register

--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -1,12 +1,12 @@
 const { Router } = require("express")
 const { registerDoctor, getAllDoctors, getDoctorById, updateDoctor } = require("../controller/doctorController")
-const { authorizationMiddlewareForRole2 } = require('../middleware/authorizationMiddleware')
+const authenticationMiddleware = require('../middleware/authenticationMiddleware');
 
 const doctorRouter = Router()
 
-doctorRouter.post("/register", authorizationMiddlewareForRole2, registerDoctor)
-doctorRouter.get("/getAll", getAllDoctors)
-doctorRouter.get("/getById/:dok_id", getDoctorById)
-doctorRouter.put("/update/:dok_id", updateDoctor)
+doctorRouter.post("/register", registerDoctor)
+doctorRouter.get("/getAll", authenticationMiddleware, getAllDoctors)
+doctorRouter.get("/getById/:dok_id",authenticationMiddleware, getDoctorById)
+doctorRouter.put("/update/:dok_id", authenticationMiddleware, updateDoctor)
 
 module.exports = doctorRouter
