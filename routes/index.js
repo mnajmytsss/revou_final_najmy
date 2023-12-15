a1104496f03d02e48ad6aa6254cfbf89ecd899b9
mnajmytsss <najmytsaqiby22@gmail.com>
2023-12-15 19:48:59 +0700

    fix: remove authorization doctor informer register

--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,18 @@ const actDoctorRouter = require("./activateDoctorAccount");
 const deactDoctorRouter = require("./deactivateDoctorAccount");
 const informerRouter = require("./informerRoutes")
 const imagesRouter = require("./uploadImagesRoutes")
+const authGoogleRouter = require("./authGoogleRoutes")
 
 const router = Router();
 
 router.use("/", dashboardRoutes);
 router.use("/api/v1/attack", preventAttackController)
 router.use("/api/v1/auth", authRouter)
-router.use("/api/v1/doctor", authenticationMiddleware, doctorRouter)
+router.use("/api/v1", authGoogleRouter)
+router.use("/api/v1/doctor", doctorRouter)
 router.use("/api/v1/admin", authenticationMiddleware, actDoctorRouter)
 router.use("/api/v1/admin", authenticationMiddleware, deactDoctorRouter)
-router.use("/api/v1/informer", authenticationMiddleware, informerRouter)
+router.use("/api/v1/informer", informerRouter)
 router.use("/api/v1/image",authenticationMiddleware, imagesRouter)
 
 module.exports = router;
